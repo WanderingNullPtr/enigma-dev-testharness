8f5c93997d330f258831624cf8e2c890f0eb7499
WanderingNullPtr <nb.mithai@gmail.com>
2022-09-03 15:20:27 +0530

    Full Pipeline x11-utils Beta.03_RC_01 TRACED

--- generate_report.sh
+++ generate_report.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-head="<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"reportstyle.css\"><script src=\"reportparser.js\" type=\"text/javascript\"></script></head><script id=\"testreportXML\" type=\"text/xml\"><?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+head="<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"reportstyle.css\"><script src=\"reportparser.js\" type=\"text/javascript\"></script><script id=\"testreportXML\" type=\"text/xml\"><?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
-tail="</script><body onload=\"loader()\"></body></html>"
+tail="</script></head><body onload=\"loader()\"></body></html>"
 
 sed "1s|.*|$head|" test-harness-out/TestReport.xml > test-harness-out/report.html
 echo $tail >> test-harness-out/report.html
